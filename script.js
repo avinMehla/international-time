0585e4cf824c7cc3683f90b44f6463b91ebd4801
Avin Mehla <Egselfish123@gmail.com>
2025-03-10 16:04:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+async function getTime(){
+    let button = document.getElementById("finder");
+    
+    // Add rotation effect
+    button.classList.add("rotating");
+
+    setTimeout(() => {
+        button.classList.remove("rotating");
+    }, 3000);
+
+    const location = document.getElementById("location").value;
+    const answer = document.getElementById("response");
+   const API_key = "c9ea3a9ed181760d48475ff59c0c5a8d";
+    
+        const jgah =  await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=5&appid=${API_key}`);
+        const place =  await jgah.json();
+        const x = place[0]["lat"];
+        const y = place[0]["lon"];
+        console.log("lattitude and longitude are ");
+        const time =  await fetch(`https://timeapi.io/api/time/current/coordinate?latitude=${x}&longitude=${y}`);
+        const final =  await time.json();
+        const samay = final["time"]; 
+       answer.innerHTML= ` Currently its : ${samay} in ${location} (24 hr clock)
+       with Date : ${final["date"]} (MM/DD/YYYY) Day : ${final["dayOfWeek"]} `;
+       
+       console.log(samay)
+    
+
+   
+}
+
+    
+
