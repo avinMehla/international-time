f98c73636d75f2a9f787354b9002ac977041b0c4
Avin Mehla <Egselfish123@gmail.com>
2025-03-10 19:41:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+import Api_Key;
 async function getTime(){
     let button = document.getElementById("finder");
     
@@ -10,8 +11,9 @@ async function getTime(){
 
     const location = document.getElementById("location").value;
     const answer = document.getElementById("response");
+    
     try{
-        const jgah =  await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=5&appid=c9ea3a9ed181760d48475ff59c0c5a8d`);
+        const jgah =  await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=5&appid=${Api_Key}`);
         const place =  await jgah.json();
         const x = place[0]["lat"];
         const y = place[0]["lon"];
